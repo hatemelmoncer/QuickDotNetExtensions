a1d786a82b5c0d02d0df1cb8bb4efc8eb8551594
Hatem El Moncer <hatemelmoncer@gmail.com>
2025-08-11 21:58:58 +0200

    DateTime and DateOnly extensions.

---
+++ QuickDotNetExtensions/DateOnlyExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace QuickDotNetExtensions;
+
+public static class DateOnlyExtensions
+{
+    public static DateTime ToDateTime(this DateOnly dateOnly)
+    {
+        return dateOnly.ToDateTime(default);
+    }
+
+    public static string ToYYYYMMDD(this DateOnly dateOnly)
+    {
+        return dateOnly.ToString("yyyyMMdd");
+    }
+
+    public static DateOnly Yesterday(this DateOnly dateOnly)
+    {
+        return dateOnly.AddDays(-1);
+    }
+
+    public static DateOnly YesterdayAndSkipWeekend(this DateOnly dateOnly)
+    {
+        var yesterday = dateOnly.AddDays(-1);
+
+        if (yesterday.DayOfWeek == DayOfWeek.Saturday)
+            return yesterday.Yesterday();
+
+        if (yesterday.DayOfWeek == DayOfWeek.Sunday)
+            return yesterday.Yesterday().Yesterday();
+
+        return yesterday;
+    }
+
+    public static DateOnly Tomorrow(this DateOnly dateOnly)
+    {
+        return dateOnly.AddDays(1);
+    }
+
+    public static DateOnly TomorrowAndSkipWeekend(this DateOnly dateOnly)
+    {
+        var tomorrow = dateOnly.AddDays(1);
+
+        if (tomorrow.DayOfWeek == DayOfWeek.Saturday)
+            return tomorrow.Tomorrow().Tomorrow();
+
+        if (tomorrow.DayOfWeek == DayOfWeek.Sunday)
+            return tomorrow.Tomorrow();
+
+        return tomorrow;
+    }
+}
---
+++ QuickDotNetExtensions/DateTimeExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace QuickDotNetExtensions;
+
+public static class DateTimeExtensions
+{
+    public static DateOnly ToDateOnly(this DateTime dateTime)
+    {
+        return DateOnly.FromDateTime(dateTime);
+    }
+
+    public static string ToYYYYMMDD(this DateTime dateTime)
+    {
+        return dateTime.ToString("yyyyMMdd");
+    }
+
+    public static DateTime Yesterday(this DateTime dateTime)
+    {
+        return dateTime.AddDays(-1);
+    }
+
+    public static DateTime YesterdayAndSkipWeekend(this DateTime dateTime)
+    {
+        var yesterday = dateTime.AddDays(-1);
+
+        if (yesterday.DayOfWeek == DayOfWeek.Saturday)
+            return yesterday.Yesterday();
+
+        if (yesterday.DayOfWeek == DayOfWeek.Sunday)
+            return yesterday.Yesterday().Yesterday();
+
+        return yesterday;
+    }
+
+    public static DateTime Tomorrow(this DateTime dateTime)
+    {
+        return dateTime.AddDays(1);
+    }
+
+    public static DateTime TomorrowAndSkipWeekend(this DateTime dateTime)
+    {
+        var tomorrow = dateTime.AddDays(1);
+
+        if (tomorrow.DayOfWeek == DayOfWeek.Saturday)
+            return tomorrow.Tomorrow().Tomorrow();
+
+        if (tomorrow.DayOfWeek == DayOfWeek.Sunday)
+            return tomorrow.Tomorrow();
+
+        return tomorrow;
+    }
+}
