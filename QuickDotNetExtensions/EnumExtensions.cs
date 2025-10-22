e5a21afc9cdb764a280d0d6b16b55eaae7ce4761
Hatem El Moncer <hatemelmoncer@gmail.com>
2025-10-22 06:02:06 +0200

    C# 10 top level statements.

--- QuickDotNetExtensions/EnumExtensions.cs
+++ QuickDotNetExtensions/EnumExtensions.cs
@@ -1,28 +1,27 @@
 ﻿using System.ComponentModel;
 using System.Reflection;
 
-namespace QuickDotNetExtensions
+namespace QuickDotNetExtensions;
+
+public static class EnumExtensions
 {
-    public static class EnumExtensions
+    /// <summary>
+    /// Returns the Description atttribute value if exists, otherwise apply ToString() to the enum field
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static string GetDescription(this Enum source)
     {
-        /// <summary>
-        /// Returns the Description atttribute value if exists, otherwise apply ToString() to the enum field
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static string GetDescription(this Enum source)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
 
-            FieldInfo fieldInfo = source.GetType().GetField(source.ToString())!;
-            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+        FieldInfo fieldInfo = source.GetType().GetField(source.ToString())!;
+        DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
 
-            if (attributes != null && attributes.Length > 0)
-                return attributes[0].Description;
-            else
-                return source.ToString();
-        }
+        if (attributes != null && attributes.Length > 0)
+            return attributes[0].Description;
+        else
+            return source.ToString();
     }
 }
